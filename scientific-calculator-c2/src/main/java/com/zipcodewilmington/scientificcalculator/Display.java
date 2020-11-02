3cf61520b69543c84c1276751de71c976f3dd520
Will Miller <70451015+will-milleriii@users.noreply.github.com>
2020-11-02 08:18:41 -0500

    Add files via upload

--- scientific-calculator-c2/src/main/java/com/zipcodewilmington/scientificcalculator/Display.java
+++ scientific-calculator-c2/src/main/java/com/zipcodewilmington/scientificcalculator/Display.java
@@ -1,41 +1,55 @@
 package com.zipcodewilmington.scientificcalculator;
+import java.util.Random;
 import java.util.Scanner;
-import java.lang.String;
+//import java.lang.String;
 public class Display {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ScientificFunctions sci = new ScientificFunctions();
         sci.displayMode = "decimal";
         sci.unitsMode = "degrees";
         Scanner in = new Scanner(System.in);
-
-        System.out.println("Choose between binary, decimal, hexadecimal, and octal");
-
-        //User input
-        String userInput = in.nextLine();
-
-        if (in.equals("binary")){
-            sci.displayMode = "binary";
-        }else if (in.equals("decimal")) {
-            sci.displayMode = "decimal";
-        }else if (in.equals("hexadecimal")) {
-            sci.displayMode = "hexadecimal";
-        }else if (in.equals("octal")){
-            sci.displayMode = "octal;";
-        }
-
-        String decimal = userInput;
-        String binary;
-        binary = Integer.toBinaryString(Integer.parseInt(decimal));
-
-        String octal;
-        octal = Integer.toOctalString(Integer.parseInt(decimal));
-
-        String hexadecimal;
-        hexadecimal = Integer.toOctalString(Integer.parseInt(decimal));
-
-        //Output
-        System.out.println();
+        int decimal;
     }
 
+    public static void getToBinaryString() {
+        int number, i = 0;
+        int binary[] = new int[100];
+        Scanner user_input = new Scanner(System.in);
+        System.out.print("Enter a value");
+        number = user_input.nextInt();
+
+        while (number != 0) {
+            binary[i] = number % 2;
+            number = number / 2;
+            i++;
+        }
+        System.out.print("Binary value = ");
+        for (int j = 1 - j; i >= 0; i--)
+            System.out.print("" + binary[j]);
+    }
+
+
+    public static void getToDecimalString() {
+    }
+
+    public static void getToOctalString() {
+
+    }
+
+    public static void hexadecimal() {
+        Scanner user_input =new Scanner(System.in);
+        int decimal, remainder;
+        System.out.print("Enter a value: ");
+        char[] hexa = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        String hexaValue = "";
+
+        while(decimal > 0){
+            remainder= decimal%16;
+            hexaValue = hexa[remainder] + hexaValue;
+            decimal = decimal/16;
+
+        }
+        System.out.print("Hexa value = "+);
+    }
 }
--- scientific-calculator-c2/src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ scientific-calculator-c2/src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -16,54 +16,54 @@ public class MainApplication {
 
         String quitOrNo = "";
         do {
-            Integer i = Console.getIntegerInput("basic arithmetic/functions: 1\nscientific operations: 2\ncustom features: 3");
+            Integer i = Console.getIntegerInput("basic arithmetic/functions: 1\nscientific operations: 2\ncustom features: 3\ndisplay mode: 4");
             switch (i) {
                 case 1:
-                Integer j = Console.getIntegerInput("addition: 1\nsubtraction: 2\nmultiplication: 3\ndivision: 4\n" +
-                        "x^2: 5\nsquare root: 6\nx^n: 7\nget inverse: 8\nget positive and negative of input: 9");
-                switch (j) {
-                    case 1:
-                        // call function to do math
-                        BasicArithmetic.getAdditionInputs();
-                        break;
+                    Integer j = Console.getIntegerInput("addition: 1\nsubtraction: 2\nmultiplication: 3\ndivision: 4\n" +
+                            "x^2: 5\nsquare root: 6\nx^n: 7\nget inverse: 8\nget positive and negative of input: 9");
+                    switch (j) {
+                        case 1:
+                            // call function to do math
+                            BasicArithmetic.getAdditionInputs();
+                            break;
                         // call function to display
                         // "Display.display();"
-                    case 2:
-                        BasicArithmetic.getSubtractionInputs();
-                        break;
-                    case 3:
-                        BasicArithmetic.getMultiplicationInputs();
-                        break;
-                    case 4:
-                        BasicArithmetic.getDivisionInputs();
-                        break;
-                    case 5:
-                        AdvArithmetic.getSquareInput();
-                        break;
-                    case 6:
-                        AdvArithmetic.getSqrtInput();
-                        break;
-                    case 7:
-                        AdvArithmetic.getPowInput();
-                        break;
-                    case 8:
-                        AdvArithmetic.getInverseFunctionInput();
-                        break;
-                    case 9:
-                        AdvArithmetic.getPositiveAndNegativeInput();
-                        break;
-                    default:
-                        Console.println("invalid input please try again.");
-                        break;
+                        case 2:
+                            BasicArithmetic.getSubtractionInputs();
+                            break;
+                        case 3:
+                            BasicArithmetic.getMultiplicationInputs();
+                            break;
+                        case 4:
+                            BasicArithmetic.getDivisionInputs();
+                            break;
+                        case 5:
+                            AdvArithmetic.getSquareInput();
+                            break;
+                        case 6:
+                            AdvArithmetic.getSqrtInput();
+                            break;
+                        case 7:
+                            AdvArithmetic.getPowInput();
+                            break;
+                        case 8:
+                            AdvArithmetic.getInverseFunctionInput();
+                            break;
+                        case 9:
+                            AdvArithmetic.getPositiveAndNegativeInput();
+                            break;
+                        default:
+                            Console.println("invalid input please try again.");
+                            break;
                     }
-                break;
+                    break;
                 case 2:
-                // Console.println("Still working on that!");
-                Console.println("Which operation would you like to perform?");
-                Integer scientificOperationMenu = Console.getIntegerInput("sine: 1\ncosine: 2\ntangent: 3\nsine inverse: 4\n" +
-                        "cosine inverse: 5\ntangent inverse: 6\nlogarithm: 7\ninverse logarithm: 8\nnatural logarithm 9\n" +
-                        "inverse natural logarithm: 10\nfactorial: 11");
-                float input = 0;
+                    // Console.println("Still working on that!");
+                    Console.println("Which operation would you like to perform?");
+                    Integer scientificOperationMenu = Console.getIntegerInput("sine: 1\ncosine: 2\ntangent: 3\nsine inverse: 4\n" +
+                            "cosine inverse: 5\ntangent inverse: 6\nlogarithm: 7\ninverse logarithm: 8\nnatural logarithm 9\n" +
+                            "inverse natural logarithm: 10\nfactorial: 11");
+                    float input = 0;
                     switch (scientificOperationMenu) {
                         case 1:
                             // sine
@@ -118,33 +118,57 @@ public class MainApplication {
                         case 11:
                             // factorial
                             input = Console.getIntegerInput("enter the number you'd like the factorial of:");
-                            Console.println(String.valueOf(factorial(input)));
+                            Console.println(String.valueOf(factorial((int) input)));
                             break;
                         default:
-                        // error message repeat prompt
+                            // error message repeat prompt
                             Console.println("invalid input please try again.");
                             break;
                     }
                     break;
                 case 3:
-                // Console.println("Still working on that!");
-                Console.println("Which special function would you like to perform?");
-                Integer customFeatureMenu = Console.getIntegerInput("your dog's age in human years: 1\nyour cat's age in human years: 2");
+                    // Console.println("Still working on that!");
+                    Console.println("Which special function would you like to perform?");
+                    Integer customFeatureMenu = Console.getIntegerInput("your dog's age in human years: 1\nyour cat's age in human years: 2");
 
-                switch (customFeatureMenu) {
-                    case 1:
-                        getDogToHumanAgeInput();
-                        break;
-                    case 2:
-                        getCatToHumanAgeInput();
-                        break;
-                    default:
-                        Console.println("invalid command please try again.");
-                }
-                break;
+                    switch (customFeatureMenu) {
+                        case 1:
+                            getDogToHumanAgeInput();
+                            break;
+                        case 2:
+                            getCatToHumanAgeInput();
+                            break;
+                        default:
+                            Console.println("invalid command please try again.");
+                    }
+                    break;
+
+                case 4:
+                    Console.println("What would you like to convert your value to?");
+                    Integer decimalConversionMenu = Console.getIntegerInput("decimal: 1\nbinary 2\noctal 3\nhexadecimal 4");
+
+                    switch (decimalConversionMenu) {
+                        case 1:
+                            //To decimal
+                            Display.getToDecimalString();
+                            break;
+
+                        case 2:
+                            //To binary
+                            Display.getToBinaryString();
+
+                        case 3:
+                            //To octal
+                            Display.getToOctalString();
+
+                        case 4:
+                            //To hexadecimal
+                            Display.hexadecimal();
+                    }
+
                 default:
-                Console.println("Invalid command, try that again");
-                break;
+                    Console.println("Invalid command, try that again");
+                    break;
             }
 
 
